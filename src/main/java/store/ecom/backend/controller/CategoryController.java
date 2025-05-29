05ce562e8f6cae9ffd48d2cbf686efb1d9506477
Hải Tân <221124022310@due.udn.vn>
2025-05-29 14:10:17 +0700

    Hai commit v3

--- src/main/java/store/ecom/backend/controller/CategoryController.java
+++ src/main/java/store/ecom/backend/controller/CategoryController.java
@@ -1,5 +1,92 @@
 package store.ecom.backend.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import store.ecom.backend.exceptions.AlreadyExistsException;
+import store.ecom.backend.exceptions.ResourceNotFoundException;
+import store.ecom.backend.model.Category;
+import store.ecom.backend.response.ApiResponse;
+import store.ecom.backend.service.category.CategoryServiceImpl;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("${api.prefix}/categories")
 public class CategoryController {
-  
+
+  private final CategoryServiceImpl categoryService;
+
+  @GetMapping("/all")
+  public ResponseEntity<ApiResponse> getAllCategories() {
+    try {
+      List<Category> categories = categoryService.getAllCategories();
+      return ResponseEntity.ok(new ApiResponse("Lấy dữ liệu thành công", categories));
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .body(new ApiResponse("Lỗi lấy danh sách danh mục sản phẩm" + e.getMessage(), null));
+    }
+  }
+
+  @PostMapping("/add")
+  public ResponseEntity<ApiResponse> addCategory(@RequestBody Category name) {
+    try {
+      Category theCategory = categoryService.addCategory(name);
+      return ResponseEntity.status(HttpStatus.CREATED)
+          .body(new ApiResponse("Thêm danh mục thành công", theCategory));
+    } catch (AlreadyExistsException e) {
+      return ResponseEntity.status(HttpStatus.CONFLICT)
+          .body(new ApiResponse(e.getMessage(), null));
+    }
+  }
+
+  @GetMapping("/category/{id}/category")
+  public ResponseEntity<ApiResponse> getCategoryById(@PathVariable Long id) {
+    try {
+      Category thCategory = categoryService.getCategoryById(id);
+      return ResponseEntity.ok(new ApiResponse("Lấy danh mục thành công", thCategory));
+    } catch (ResourceNotFoundException e) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+    }
+  }
+
+  @GetMapping("/category/{name}/category")
+  public ResponseEntity<ApiResponse> getCategoryByName(@PathVariable String name) {
+    try {
+      Category thCategory = categoryService.getCategoryByName(name);
+      return ResponseEntity.ok(new ApiResponse("Lấy danh mục thành công", thCategory));
+    } catch (ResourceNotFoundException e) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+    }
+  }
+
+  @DeleteMapping("/category/{id}/delete")
+  public ResponseEntity<ApiResponse> deleteCategory(@PathVariable Long id) {
+    try {
+      categoryService.deleteCategoryById(id);
+      return ResponseEntity.ok(new ApiResponse("Xoá danh mục thành công", null));
+    } catch (ResourceNotFoundException e) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+    }
+  }
+
+  @PutMapping("/category/{id}/update")
+  public ResponseEntity<ApiResponse> updateCategory(@PathVariable Long id, @RequestBody Category category) {
+    try {
+      Category updatedCategory = categoryService.updateCategory(category, id);
+      return ResponseEntity.ok(new ApiResponse("Cập nhật danh mục thành công", updatedCategory));
+    } catch (ResourceNotFoundException e) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+    }
+  }
 }
